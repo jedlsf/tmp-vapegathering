19b1a9710a91ae14c6d5b0d67625511dac404088
Zelijah <zeffabian@gmail.com>
2024-07-28 17:48:00 +0800

    new favicon and page title

--- src/assets-imported/assets.js
+++ src/assets-imported/assets.js
@@ -1,5 +1,7 @@
 import logoEmblem from './logo_VG.png';
+import faviconRoundAlpha from './FavIcon_RoundAlpha.png';
 
 
 
 export const logoVG = logoEmblem;
+export const faviconRound = faviconRoundAlpha;
