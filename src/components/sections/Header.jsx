22dac30b06b017fb495a33e222394e15bc664229
Zelijah <zeffabian@gmail.com>
2024-08-07 22:04:03 +0800

    demo header modified update

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { logoVG } from '../../assets-imported/assets';
 import NavMenuTab from '../functional/NavMenuTab';
@@ -31,10 +31,20 @@ const Logo = styled.img`
 
 
 function Header() {
+
+  const [showNavMenu, setShowNavMenu] = useState(false);
+
+  useEffect(() => {
+    const currentDomain = window.location.hostname;
+    if (currentDomain !== 'vapegathering.com') {
+      setShowNavMenu(true);
+    }
+  }, []);
+
   return (
     <HeaderContainer>
       <Logo src={logoVG} className="App-logo" alt="logo" />
-
+      {showNavMenu && <NavMenuTab />}
     </HeaderContainer>
   );
 }
