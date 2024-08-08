38e01e636fbb94d60d2347d564223ea070ef4001
Zelijah <zeffabian@gmail.com>
2024-08-08 16:10:23 +0800

    updated page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import CountdownTimer from '../components/functional/CountdownTimer';
 import ThreeDViewer from '../components/3d/ThreeDViewer';
 import VFXCircleMask from '../components/vfx/VFXCircleMask';
@@ -28,19 +28,87 @@ const RevealedComponentContainer = styled.div`
   z-index: 0; /* Ensure it is behind other content */
 `;
 
+const gradientAnimation = keyframes`
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+`;
+
+const CenteredRevealedComponent = styled.div`
+  position: absolute;
+  width: 330px;
+  height: 210px;
+  top: 40%;
+  left: 50%;
+  border-radius: 15px;
+ 
+  transform: translate(-50%, -50%);
+  background: linear-gradient(
+    45deg, 
+    ${({ theme }) => theme.colors.primaryBackground},
+    ${({ theme }) => theme.colors.secondaryBackground}, 
+    ${({ theme }) => theme.colors.brand.red},
+    ${({ theme }) => theme.colors.brand.blue},
+    ${({ theme }) => theme.colors.brand.yellow},
+    ${({ theme }) => theme.colors.brand.green},
+    ${({ theme }) => theme.colors.brand.orange},
+    ${({ theme }) => theme.colors.brand.pink}
+  );
+  background-size: 200% 200%;
+  filter: 
+  animation: ${gradientAnimation} 4s ease infinite;
+  background-blend-mode: overlay;
+  display: ${({ isVisible }) => (isVisible ? 'block' : 'none')}; /* Show/hide based on hover state */
+  z-index: 10; /* Ensure it is on top of other content */
+  pointer-events: none; /* Allow mouse events to pass through */
+  overflow: hidden;
+`;
+
+const TextContent = styled.div`
+  padding: 15px;
+  color: ${({ theme }) => theme.colors.textPrimary};
+  font-size: 20px;
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 90%;
+
+  backdrop-filter: blur(45px);
+  box-shadow: inset 0px 3px 7px rgba(0, 0, 0, 0.4);
+`;
+
 const path1 = 'M 0 0 Q 600 100 250 200 Q 0 250 150 550 Q 400 850 850 700 Q 1300 500 1200 300 Q 1000 50 1550 50 Q 1900 50 1700 650 Q 1500 1050 1050 1050 ';
 const path2 = 'M 0 600 Q 500 1100 1250 850 C 1800 550 650 300 1900 0 ';
 const path3 = "M 300 0 Q 500 1100 1000 950 C 1900 400 0 200 1900 250 ";
 const targetDate = '2024-09-16T00:00:00Z';
+
+const textContentArray = [
+    'Presented by TMP Productions, Inc.',
+    'September 28, 2024',
+    'Available Soon',
+    'The Tent - Las Pinas, Metro Manila',
+    'The Vape Gathering is where the vape community meets and has fun; a social gathering and forum where we recognize the legitimacy of the vape industry. Here, the community can see new products, meet the brands, get exclusive insights, and connect with people who share the same enthusiasm towards vaping.',
+];
 
 function Home() {
     const [hovered, setHovered] = useState(false);
     const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 150 });
     const [backgroundColor, setBackgroundColor] = useState(theme.colors.primaryBackground);
     const [revealedComponent, setRevealedComponent] = useState(null);
+    const [centeredComponentVisible, setCenteredComponentVisible] = useState(false);
+    const [centeredText, setCenteredText] = useState('');
 
     const handleHovered = (index) => {
         setHovered(true);
+        setCenteredText(textContentArray[index] || textContentArray[4]);
+        setCenteredComponentVisible(true);
         if (index === 0) {
             revealComponent(<AnimatedSplinePath
                 path={path1}
@@ -97,15 +165,23 @@ function Home() {
     const handleHoverEnd = () => {
         setHovered(false);
         changeBackgroundColor(theme.colors.primaryBackground);
+        setCenteredComponentVisible(false);
         setRevealedComponent(null);
     };
 
     const handleMouseMove = (event) => {
+
         setCursorPosition({
             x: event.clientX,
             y: event.clientY - 100,
         });
+
     };
+
+
+
+
+
 
     const changeBackgroundColor = (color) => {
         setBackgroundColor(color);
@@ -120,11 +196,16 @@ function Home() {
             <RevealedComponentContainer isVisible={hovered}>
                 {revealedComponent}
             </RevealedComponentContainer>
+
             <VFXCircleMask isVisible={hovered} cursorPosition={cursorPosition} />
             <ThreeDViewer
                 isHovered={handleHovered}
                 isHoverEnd={handleHoverEnd}
             />
+            <CenteredRevealedComponent isVisible={centeredComponentVisible}>
+
+                <TextContent>{centeredText}</TextContent>
+            </CenteredRevealedComponent>
             <CountdownTimer targetDate={targetDate} />
         </Container>
     );
