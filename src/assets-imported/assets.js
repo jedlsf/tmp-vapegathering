04505533d70558fae787811ee2b07e425af6ef5f
Zelijah <zeffabian@gmail.com>
2024-08-09 14:33:51 +0800

    added placeholder event layout

--- src/assets-imported/assets.js
+++ src/assets-imported/assets.js
@@ -3,6 +3,7 @@ import faviconRoundAlpha from './FavIcon_RoundAlpha.png';
 import pceiaPng from './LogoPCEIA.png';
 import age18pPng from './Logo18p.png';
 import venueImage from './imgTheTent.jpg';
+import eLayoutImg from './eLayout.png';
 
 
 //Import images for Past Events slider
@@ -61,6 +62,7 @@ export const faviconRound = faviconRoundAlpha;
 export const logoPCEIA = pceiaPng;
 export const logo18p = age18pPng;
 export const imageVenue = venueImage;
+export const eLayoutImage = eLayoutImg;
 
 
 export const listSlidePastEvents = [
--- src/pages/EventLayout.jsx
+++ src/pages/EventLayout.jsx
@@ -1,54 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
-import EventMapLayout from '../components/3d/EventMapLayout';
+import BentoBlock from '../components/foundations/BentoBlock';
+import ImageSliderScroll from '../components/functional/ImageSliderScroll';
+import AnimatedSplinePath from '../components/vfx/AnimatedSplinePath';
 import theme from '../theme.js';
+import { eLayoutImage } from '../assets-imported/assets.js';
 
 const MainContainer = styled.div`
-  width: 90vw;
-  height: 70vh;
-  position: relative;
-  z-index: 4;
-  backdrop-filter: blur(25px);
-  margin: 25px;
- 
+  padding: 20px;
+  width: 100vw;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: transparent;
+  box-sizing: border-box;
+  overflow: auto;
 `;
 
 const BodyContainer = styled.div`
-  width: 70%;
-  max-width: 350px;
-  height: 70vh;
-  padding: 20px;
-  display: flex;
-  position: relative;
-  flex-direction: column;
-  align-items: center;
-  box-shadow: inset 0px 0px 7px rgba(0, 0, 0, 0.3);
-  background-color: ${({ theme }) => theme.colors.primaryBackground};
-  gap: 15px;
-  box-sizing: border-box;
-  margin-right:15px;
-   margin-left:150px;
-
-  border-radius: 15px;
-`;
-
-const InfoContainer = styled.div`
-  width: 70%;
+  width: 100%;
   max-width: 1400px;
-  height: 70vh;
   padding: 20px;
   display: flex;
-  position: relative;
   flex-direction: column;
   align-items: center;
   box-shadow: inset 0px 0px 7px rgba(0, 0, 0, 0.3);
-  background-color: ${({ theme }) => theme.colors.primaryBackground};
+  background-color: ${({ theme }) => theme.colors.secondaryBackground};
   gap: 15px;
   box-sizing: border-box;
-  margin-right:15px;
-   margin-left:150px;
-
+  height: auto; /* Allow height to adjust based on content */
   border-radius: 15px;
+  opacity: 0.97;
 `;
 
 const DynaContainer = styled.div`
@@ -79,19 +62,66 @@ const ColumnContainer = styled.div`
   }
 `;
 
+const FullWidthBentoBlock = styled(BentoBlock)`
+  width: 100%;  /* Ensure it takes the full width */
+`;
+
+const BgOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+`;
 
 
-function EventLayout() {
-    return (
-        <MainContainer>
-            <DynaContainer>
-                <BodyContainer>
-                    <EventMapLayout />
-                </BodyContainer>
-                <InfoContainer />
-            </DynaContainer>
-        </MainContainer>
-    );
+const ContentContainer = styled.div`
+  position: relative;
+  z-index: 10;
+   width: 100%;
+  max-width: 1400px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: inset 0px 0px 7px rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+`;
+
+const PlaceholderImage = styled.img`
+width: 100%;
+height: 100%;
+
+`;
+
+const path3 = "M 300 0 Q 500 1100 1000 950 C 1900 400 0 200 1900 250 ";
+
+function Awards() {
+  return (
+    <MainContainer>
+      <BgOverlay>
+        <AnimatedSplinePath
+          path={path3}
+          lineColor={theme.colors.brand.yellow}
+          sWidth={155}
+          speed={15}
+          gColor1={theme.colors.brand.red}
+          gColor2={theme.colors.brand.orange}
+          gColor3={theme.colors.brand.yellow}
+        />
+      </BgOverlay>
+      <ContentContainer>
+        <BodyContainer>
+          <PlaceholderImage
+            src={eLayoutImage}
+          />
+
+
+        </BodyContainer>
+      </ContentContainer>
+    </MainContainer>
+  );
 }
 
-export default EventLayout;
+export default Awards;
